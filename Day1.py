2a675baadd7754991974487c94cd0e03ae624302
Pratiksha Doundkar <90963603+PratikshaDoundkar@users.noreply.github.com>
2023-06-08 11:33:57 +0530

    Create Day1.py

---
+++ Day1.py
@@ -0,0 +1,16 @@
+#Write a program that prints the number of characters in a user's name. You might need to Google for a function that calculates the length of a string
+s = input("What is your name?")
+print(len(s))
+
+#Write a program that switches the values stored in the variables a and b
+
+a = input("a: ")
+b = input("b: ")
+c=a
+a=b
+b=c
+print("a: " + a)
+print("b: " + b)
+
+#
+
